681c6bd990d634144b2f4c6f30a5ad88264f7eca
Nitesh Nandan <talk2nandan5686@gmail.com>
2019-07-24 18:50:13 +0530

    React Hello World

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,8 @@
 console.log("App is working!!");
+
+// const template = <h1>Hello world!!</h1>;
+
+const template = React.createElement("h1", null, "Hello world!!");
+
+
+ReactDOM.render(template, document.getElementById("App"));
